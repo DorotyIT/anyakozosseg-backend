0537df107d1af22f69121f1ca95bdd7be0dfd313
DorotyIT <dorottya1203@gmail.com>
2023-03-16 12:08:34 +0100

    add ratings table to the database

--- Database/mother_community.sql
+++ Database/mother_community.sql
@@ -1,34 +1,16 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Már 15. 14:27
--- Kiszolgáló verziója: 10.4.27-MariaDB
--- PHP verzió: 8.2.0
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Adatbázis: `mother_community`
---
 CREATE DATABASE IF NOT EXISTS `mother_community` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `mother_community`;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `brands`
---
-
+DROP TABLE IF EXISTS `brands`;
 CREATE TABLE IF NOT EXISTS `brands` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -39,14 +21,9 @@ CREATE TABLE IF NOT EXISTS `brands` (
   `price_category_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `price_category_id` (`price_category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `categories`
---
-
+DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -54,22 +31,13 @@ CREATE TABLE IF NOT EXISTS `categories` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- A tábla adatainak kiíratása `categories`
---
-
 INSERT INTO `categories` (`id`, `name`, `image_path`) VALUES
 (1, 'SZÉPSÉGÁPOLÁS', 'assets/card_szepsegapolas.jpg'),
 (2, 'HÁZTARTÁS', 'assets/card_haztartas.jpg'),
 (3, 'BIO TERMÉKEK', 'assets/card_bio_termekek.jpg'),
 (4, 'ÉTREND-KIEGÉSZÍTŐK', 'assets/card_etrend_kiegeszitok.jpg');
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `categories_to_brands`
---
-
+DROP TABLE IF EXISTS `categories_to_brands`;
 CREATE TABLE IF NOT EXISTS `categories_to_brands` (
   `brand_id` bigint(20) NOT NULL,
   `category_id` bigint(20) NOT NULL,
@@ -77,12 +45,7 @@ CREATE TABLE IF NOT EXISTS `categories_to_brands` (
   KEY `category_id` (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `categories_to_ingredients`
---
-
+DROP TABLE IF EXISTS `categories_to_ingredients`;
 CREATE TABLE IF NOT EXISTS `categories_to_ingredients` (
   `ingredient_id` bigint(20) NOT NULL,
   `category_id` bigint(20) NOT NULL,
@@ -90,12 +53,7 @@ CREATE TABLE IF NOT EXISTS `categories_to_ingredients` (
   KEY `category_id` (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `categories_to_products`
---
-
+DROP TABLE IF EXISTS `categories_to_products`;
 CREATE TABLE IF NOT EXISTS `categories_to_products` (
   `product_id` bigint(20) NOT NULL,
   `category_id` bigint(20) NOT NULL,
@@ -103,12 +61,7 @@ CREATE TABLE IF NOT EXISTS `categories_to_products` (
   KEY `category_id` (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `ingredients`
---
-
+DROP TABLE IF EXISTS `ingredients`;
 CREATE TABLE IF NOT EXISTS `ingredients` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `label` varchar(255) NOT NULL,
@@ -118,12 +71,7 @@ CREATE TABLE IF NOT EXISTS `ingredients` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `ingredients_to_ingredient_functions`
---
-
+DROP TABLE IF EXISTS `ingredients_to_ingredient_functions`;
 CREATE TABLE IF NOT EXISTS `ingredients_to_ingredient_functions` (
   `ingredient_id` bigint(20) NOT NULL,
   `ingredient_function_id` bigint(20) NOT NULL,
@@ -131,45 +79,26 @@ CREATE TABLE IF NOT EXISTS `ingredients_to_ingredient_functions` (
   KEY `ingredient_function_id` (`ingredient_function_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `ingredient_functions`
---
-
+DROP TABLE IF EXISTS `ingredient_functions`;
 CREATE TABLE IF NOT EXISTS `ingredient_functions` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `ingredient_function` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `price_categories`
---
-
+DROP TABLE IF EXISTS `price_categories`;
 CREATE TABLE IF NOT EXISTS `price_categories` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- A tábla adatainak kiíratása `price_categories`
---
-
 INSERT INTO `price_categories` (`id`, `name`) VALUES
 (1, 'ALACSONY'),
 (2, 'KÖZEPES'),
 (3, 'MAGAS');
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `products`
---
-
+DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
   `id` bigint(20) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -180,12 +109,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `products_to_ingredients`
---
-
+DROP TABLE IF EXISTS `products_to_ingredients`;
 CREATE TABLE IF NOT EXISTS `products_to_ingredients` (
   `product_id` bigint(20) NOT NULL,
   `ingridient_id` bigint(20) NOT NULL,
@@ -193,12 +117,7 @@ CREATE TABLE IF NOT EXISTS `products_to_ingredients` (
   KEY `ingridient_id` (`ingridient_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `products_to_product_categories`
---
-
+DROP TABLE IF EXISTS `products_to_product_categories`;
 CREATE TABLE IF NOT EXISTS `products_to_product_categories` (
   `product_category_id` bigint(20) NOT NULL,
   `product_id` bigint(20) NOT NULL,
@@ -206,24 +125,26 @@ CREATE TABLE IF NOT EXISTS `products_to_product_categories` (
   KEY `product_id` (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `product_categories`
---
-
+DROP TABLE IF EXISTS `product_categories`;
 CREATE TABLE IF NOT EXISTS `product_categories` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `product_category_name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `ratings`;
+CREATE TABLE IF NOT EXISTS `ratings` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `product_id` bigint(20) NOT NULL,
+  `rating` int(5) NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `product_id` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Tábla szerkezet ehhez a táblához `users`
---
-
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
@@ -232,64 +153,40 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- A tábla adatainak kiíratása `users`
---
-
 INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
 (1, 'Mekk Elek', 'Mekmek', 'Admin');
 
---
--- Megkötések a kiírt táblákhoz
---
 
---
--- Megkötések a táblához `brands`
---
 ALTER TABLE `brands`
   ADD CONSTRAINT `brands_ibfk_1` FOREIGN KEY (`price_category_id`) REFERENCES `price_categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Megkötések a táblához `categories_to_brands`
---
 ALTER TABLE `categories_to_brands`
   ADD CONSTRAINT `categories_to_brands_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `categories_to_brands_ibfk_2` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Megkötések a táblához `categories_to_ingredients`
---
 ALTER TABLE `categories_to_ingredients`
   ADD CONSTRAINT `categories_to_ingredients_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `categories_to_ingredients_ibfk_2` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Megkötések a táblához `categories_to_products`
---
 ALTER TABLE `categories_to_products`
   ADD CONSTRAINT `categories_to_products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `categories_to_products_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Megkötések a táblához `ingredients_to_ingredient_functions`
---
 ALTER TABLE `ingredients_to_ingredient_functions`
   ADD CONSTRAINT `ingredients_to_ingredient_functions_ibfk_1` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `ingredients_to_ingredient_functions_ibfk_2` FOREIGN KEY (`ingredient_function_id`) REFERENCES `ingredient_functions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Megkötések a táblához `products_to_ingredients`
---
 ALTER TABLE `products_to_ingredients`
   ADD CONSTRAINT `products_to_ingredients_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `products_to_ingredients_ibfk_2` FOREIGN KEY (`ingridient_id`) REFERENCES `ingredients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Megkötések a táblához `products_to_product_categories`
---
 ALTER TABLE `products_to_product_categories`
   ADD CONSTRAINT `products_to_product_categories_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `products_to_product_categories_ibfk_2` FOREIGN KEY (`product_category_id`) REFERENCES `product_categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `ratings`
+  ADD CONSTRAINT `ratings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ratings_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
