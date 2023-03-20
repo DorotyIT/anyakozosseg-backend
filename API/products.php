766b6bd2df09fea95baf096956bbdbaab02b80d1
DorotyIT <dorottya1203@gmail.com>
2023-03-20 17:32:48 +0100

    add get endpoint for product-details

--- API/products.php
+++ API/products.php
@@ -60,7 +60,42 @@
                         echo json_encode($products);
                     }
 
-                    if (isset($_GET['productId'])) {}
+                    if (isset($_GET['productId'])) {
+                        $productId = $_GET['productId'];
+                        $sqlGetProduct = "SELECT * FROM `products` WHERE id={$productId}";
+
+                        $productResult = mysqli_query($connection, $sqlGetProduct);
+                        $product = mysqli_fetch_assoc($productResult);
+
+                        $sqlRatings = "SELECT *, ratings.id as rating_id FROM `ratings` JOIN users ON ratings.user_id = users.id  WHERE product_id={$productId}";
+                        $ratingsResult = mysqli_query($connection, $sqlRatings);
+                    
+                        $sqlAvgOfRatings = "SELECT AVG(rating) FROM `ratings` WHERE product_id={$productId}";
+                        $avgRatingsResult = mysqli_query($connection, $sqlAvgOfRatings);
+                        $product['avgRating'] = mysqli_fetch_assoc($avgRatingsResult)['AVG(rating)'];
+
+                        $sqlProductCategories = "SELECT product_categories.product_category_name FROM `products` JOIN products_to_product_categories ON products.id = products_to_product_categories.product_id JOIN product_categories ON product_categories.id = products_to_product_categories.product_category_id  WHERE product_id={$productId}";
+                        $productCategoriesResult = mysqli_query($connection,  $sqlProductCategories);
+                        
+                        $c = 0;
+                        while($productCategory = mysqli_fetch_assoc($productCategoriesResult)) {
+                            $product['productCategories'][$c] = $productCategory['product_category_name'];
+                            $c++;
+                        }
+
+
+                        $i = 0;
+                        while($rating = mysqli_fetch_assoc($ratingsResult)) {
+                            $product['ratings'][$i]['id'] = $rating['rating_id'];
+                            $product['ratings'][$i]['username'] = $rating['username'];
+                            $product['ratings'][$i]['rating'] = $rating['rating'];
+                            $product['ratings'][$i]['comment'] = $rating['comment'];
+                            $product['ratings'][$i]['addedOn'] = $rating['added_on'];
+                            $i++;
+                        }
+
+                        echo json_encode($product);
+                    }
                 }
                 break;
         }
