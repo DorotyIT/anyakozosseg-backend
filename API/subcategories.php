da022fa8b957dfb9c7eafaeef891270f6e528b35
DorotyIT <dorottya1203@gmail.com>
2023-04-08 12:28:28 +0200

    refactor dropdown

--- API/subcategories.php
+++ API/subcategories.php
@@ -7,15 +7,19 @@
        {
             case 'GET' : 
                 {
-                    $sqlGetProductCategories = "SELECT * FROM `subcategories`";         
-                    $result= mysqli_query($connection, $sqlGetProductCategories);
-                    
-                    if (mysqli_num_rows($result) > 0) {
-                        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-                        echo json_encode($rows);
-                    } else {
-                        echo "0 results";
+                    if (isset($_GET['categoryId'])) {
+                        $categoryId = $_GET['categoryId'];
+                        $sqlGetProductCategories = "SELECT * FROM `subcategories` WHERE category_id='{$categoryId}'";         
+                        $result= mysqli_query($connection, $sqlGetProductCategories);
+                        
+                        if (mysqli_num_rows($result) > 0) {
+                            $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                            echo json_encode($rows);
+                        } else {
+                            echo "0 results";
+                        }
                     }
+
                 }
                 break;
         }
